11affb3995cd8041982bf27de85bea3953f85f92
jeffersoncharlles <contato@jefferdeveloper.com>
2022-02-23 23:26:12 -0300

    deploy pages new

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+# set -e
 
-# build
-npm run build
+# # build
+# npm run build
 
-# navigate into the build output directory
-cd dist
+# # navigate into the build output directory
+# cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -b main
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
